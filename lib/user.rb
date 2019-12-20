28bb35c8378c610d04231dfb660d8c93d7154804
Calvin McDowell <calvinmac40@hotmail.com>
2019-12-20 01:01:29 +0000

    Automatically backed up by Learn

--- lib/user.rb
+++ lib/user.rb
@@ -2,9 +2,16 @@ class User
   
   attr_accessor :first_name, :last_name
   
+  @@all = []
+  
   def initialize(first_name,last_name)
     @first_name = first_name
     @last_name = last_name
+    save
+  end
+  
+  def save 
+    self.class.all << self
   end
   
 end
